b67f2f0d6c68ad7fedef127db021b3b7770671fd
BananaLee <benjaminzplee@gmail.com>
2022-01-25 22:12:20 +0100

    Deep Learning model finally works perfectly! Now need to run them properly with all variations.

--- Deep Learning/DL_models.py
+++ Deep Learning/DL_models.py
@@ -2,7 +2,7 @@ import os
 
 os.environ['KERAS_BACKEND'] = 'theano'
 
-from keras.layers import Dense, Flatten, Embedding, Attention
+from keras.layers import Dense, Flatten, Embedding
 from keras.layers import Conv1D, MaxPooling1D, Dropout, LSTM, Bidirectional, Layer, GlobalMaxPooling1D
 from keras.models import Sequential
 
@@ -52,13 +52,34 @@ def blstm(embed_size, num_classes, embedder):
                   metrics=['accuracy'])
     return model
 
+class attention(Layer):
+    def __init__(self,**kwargs):
+        super(attention,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")
+        super(attention, self).build(input_shape)
+
+    def call(self,x):
+        et=K.squeeze(K.tanh(K.dot(x,self.W)+self.b),axis=-1)
+        at=K.softmax(et)
+        at=K.expand_dims(at,axis=-1)
+        output=x*at
+        return K.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention,self).get_config()
 
 def blstm_att(embed_size, num_classes, embedder):
     model = Sequential()
     model.add(embedder)
     model.add(Dropout(0.25))
     model.add(Bidirectional(LSTM(embed_size, return_sequences=True)))
-    model.add(Attention(use_scale=True))
+    model.add(attention())
     model.add(Dropout(0.50))
     model.add(Dense(num_classes, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
