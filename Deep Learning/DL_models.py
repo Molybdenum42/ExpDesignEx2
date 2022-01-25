d63fcca84203b35f9a47a7e6c933784182c27690
BananaLee <benjaminzplee@gmail.com>
2022-01-25 10:24:23 +0100

    Model Improvements

--- Deep Learning/DL_models.py
+++ Deep Learning/DL_models.py
@@ -2,8 +2,8 @@ import os
 
 os.environ['KERAS_BACKEND'] = 'theano'
 
-from keras.layers import Dense, Flatten
-from keras.layers import Conv1D, MaxPooling1D, Dropout, LSTM, Bidirectional, Layer, GlobalMaxPooling1D, Embedding
+from keras.layers import Dense, Flatten, Embedding, Attention
+from keras.layers import Conv1D, MaxPooling1D, Dropout, LSTM, Bidirectional, Layer, GlobalMaxPooling1D
 from keras.models import Sequential
 
 from keras import backend as K
@@ -53,38 +53,12 @@ def blstm(embed_size, num_classes, embedder):
     return model
 
 
-class AttLayer(Layer):
-
-    def __init__(self, **kwargs):
-        super(AttLayer, self).__init__(**kwargs)
-
-    def build(self, input_shape):
-        # Create a trainable weight variable for this layer.
-        self.W = self.add_weight(name='kernel',
-                                 shape=(input_shape[-1],),
-                                 initializer='random_normal',
-                                 trainable=True)
-        super(AttLayer, self).build(input_shape)  # Be sure to call this somewhere!
-
-    def call(self, x, mask=None):
-        eij = K.tanh(K.dot(x, self.W))
-
-        ai = K.exp(eij)
-        weights = ai / K.sum(ai, axis=1).dimshuffle(0, 'x')
-
-        weighted_input = x * weights.dimshuffle(0, 1, 'x')
-        return weighted_input.sum(axis=1)
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], input_shape[-1])
-
-
 def blstm_atten(embed_size, num_classes, embedder):
     model = Sequential()
     model.add(embedder)
     model.add(Dropout(0.25))
     model.add(Bidirectional(LSTM(embed_size, return_sequences=True)))
-    model.add(AttLayer())
+    model.add(Attention())
     model.add(Dropout(0.50))
     model.add(Dense(num_classes, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
